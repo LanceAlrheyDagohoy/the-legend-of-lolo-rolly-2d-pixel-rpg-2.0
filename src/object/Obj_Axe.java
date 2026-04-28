a3bcdd17f275184afd441ca8916c0c4e318dfbc8
Lance Alrhey Dagohoy <lancedagz5@gmail.com>
2026-04-28 22:37:23 +0800

    Create Obj_Axe class with attributes and methods
    Initialize Obj_Axe with properties and attack area.

--- src/object/Obj_Axe.java
+++ src/object/Obj_Axe.java
@@ -1,1 +1,20 @@
+package object;
+
+import Entity.Entity;
+import main.GamePanel;
+
+public class Obj_Axe extends Entity {
+
+    public Obj_Axe(GamePanel gp) {
+        super(gp);
+
+        type = type_axe;
+        name = "Old Axe";
+        down1 = setup("/object/axe", gp.tileSize, gp.tileSize);
+        description = "[" + name + "]\nA cool old axe";
+        attackValue = 2;
+        attackArea.width = 30;
+        attackArea.height = 30;
+    }
+}
 

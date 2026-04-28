e70e31e78e8fb671e9fba1c16a90c2de5c9b37e8
Gian Matiga <232986032+GianMatiga@users.noreply.github.com>
2026-04-28 22:52:08 +0800

    add main sound

---
+++ src/main/Sound.java
@@ -0,0 +1,69 @@
+package main;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import java.net.URL;
+
+public class Sound {
+
+    Clip clip;
+    URL[] soundURL = new URL[30];
+    FloatControl fc;
+    int volumeScale = 3;
+    float volume;
+
+    public Sound() {
+        soundURL[0] = getClass().getResource("/sound/background.wav");
+        soundURL[1] = getClass().getResource("/sound/collect.wav");
+        soundURL[2] = getClass().getResource("/sound/pickup.wav");
+        soundURL[3] = getClass().getResource("/sound/unlock.wav");
+        soundURL[4] = getClass().getResource("/sound/win.wav");
+        soundURL[5] = getClass().getResource("/sound/hit.wav");
+        soundURL[6] = getClass().getResource("/sound/receiveDmg.wav");
+        soundURL[7] = getClass().getResource("/sound/slash.wav");
+        soundURL[8] = getClass().getResource("/sound/levelUp.wav");
+        soundURL[9] = getClass().getResource("/sound/cursor.wav");
+        soundURL[10] = getClass().getResource("/sound/heal.wav");
+        soundURL[11] = getClass().getResource("/sound/fireMagic.wav");
+        soundURL[12] = getClass().getResource("/sound/chop.wav");
+        soundURL[13] = getClass().getResource("/sound/gameOver.wav");
+
+    }
+
+    public void setFile(int i) {
+
+        try {
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
+            clip = AudioSystem.getClip();
+            clip.open(ais);
+            fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            checkVolume();
+
+        } catch ( Exception ignored) {
+
+        }
+    }
+    public void play() {
+        clip.start();
+    }
+
+    public void loop() {
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stop() { clip.stop(); }
+
+    public void checkVolume() {
+        switch (volumeScale) {
+            case 0: volume = -80f;  break;
+            case 1: volume = -20f;break;
+            case 2: volume = -12f;break;
+            case 3: volume = -5f;break;
+            case 4: volume = 1f;break;
+            case 5: volume = 6f;break;
+        }
+        fc.setValue(volume);
+    }
+}
